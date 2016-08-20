09bb41ff939a301f1ce88840951b1c21ba8293bd
atf1206 <atf1206@gmail.com>
2016-08-20 02:17:46 +0000

    Added prototype Poll schema (inactive)

--- app/models/users.js
+++ app/models/users.js
@@ -1,18 +1,46 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+//var Schema = mongoose.Schema;
 
-var User = new Schema({
+//var User = new Schema({
+var User = new mongoose.Schema({
 	github: {
 		id: String,
 		displayName: String,
 		username: String,
-      publicRepos: Number
+        publicRepos: Number
 	},
    nbrClicks: {
       clicks: Number
    }
 });
 
+/*
+var Poll = new mongoose.Schema({
+  question: {type: String, required: true},
+  pubDate: {type: Date, required: true},
+  // Can have subdocuments 
+  // (data not normalised into separate table/collection)
+  choices: [{
+    choiceText: String,
+    votes: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }]
+});
+
+Poll.methods.wasPublishedRecently = function () {
+  var now = new Date()
+  var delta = Math.abs(now.getTime() - this.pubDate.getTime())
+  // If the difference is less than
+  // the number of milliseconds in a week
+  return delta <= (1000 * 60 * 60 * 24 * 7)
+}
+
+*/
+
 module.exports = mongoose.model('User', User);
+//module.exports = mongoose.model('Poll', Poll);
